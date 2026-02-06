eb31136adaa86a9ec075d4b54130dc1176349c02
Hernan Bracamonte <hernan.bracamonte@halopowered.com>
2026-02-06 00:07:15 -0500

    fix: Correct class name for gradient background in HeroSmoothiebowl component

--- frontend/components/home/HeroSmoothiebowl.tsx
+++ frontend/components/home/HeroSmoothiebowl.tsx
@@ -37,7 +37,7 @@ export default function HeroSmoothiebowl() {
       </video>
 
       {/* Overlay - Mobile darker, Desktop lighter */}
-      <div className="absolute inset-0 bg-linear-to-b from-black/60 via-black/40 to-black/70 md:from-black/40 md:via-black/20 md:to-black/50" />
+      <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/70 md:from-black/40 md:via-black/20 md:to-black/50" />
 
       {/* Content */}
       <div className="relative h-full flex items-center justify-center px-4 sm:px-6 lg:px-8">
