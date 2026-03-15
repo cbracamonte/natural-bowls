46e85bc7051a468a5a78b4969df122ccdf82b8fe
Santiago Casana <santiagocasana@gmail.com>
2026-03-15 02:09:34 -0500

    refactor: Removed unused classes

--- backend/modules/cart/domain/cart.repository.ts
+++
@@ -1,7 +0,0 @@
-import { PoolClient } from 'pg';
-import { Cart } from './cart.entity';
-
-export interface CartRepository {
-  findActiveByCustomer(customerId: string, client?:PoolClient): Promise<Cart | null>;
-  save(cart: Cart, client?:PoolClient): Promise<void>;
-}
