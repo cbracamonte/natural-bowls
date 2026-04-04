11da3713a3c4adbb64e5653784263e99e5570b3c
Santiago Casana <santiagocasana@gmail.com>
2026-04-03 23:50:46 -0500

    refactor: Refactored DTOs and catchs

--- backend/src/modules/cart/api/dto/add-item.dto.ts
+++ backend/src/modules/cart/api/dto/add-item.dto.ts
@@ -3,9 +3,9 @@ import { IsString, IsInt, Min } from 'class-validator';
 export class AddItemDto {
 
   @IsString()
-  productId: string;
+  productId!: string;
 
   @IsInt()
   @Min(1)
-  quantity: number;
+  quantity!: number;
 }
